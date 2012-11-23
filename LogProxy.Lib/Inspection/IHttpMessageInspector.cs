e29a36832853e9ba464a746c95f4190910894e70
Konstantins Onufrijevs <koonuf@gmail.com>
2012-11-23 13:48:45 +0200

    Refactoring

---
+++ LogProxy.Lib/Inspection/IHttpMessageInspector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LogProxy.Lib.Http;
+
+namespace LogProxy.Lib.Inspection
+{
+    public interface IHttpMessageInspector : IDisposable
+    {
+        void AddRequestData(byte[] data);
+
+        void AddResponseData(byte[] data);
+
+        void RequestHeadersParsed(HttpHeadersSummary headers);
+
+        void ResponseHeadersParsed(HttpHeadersSummary headers);
+
+        void ServerReceiveFinished();
+    }
+}
